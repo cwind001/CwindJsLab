8badd8874f3a6d4b706ec8fb914373c039a0427a
Bill Chen <billchen01@163.com>
2015-05-26 18:15:52 +0800

    Login Demo

--- ExpressLoginDemo/app.js
+++ ExpressLoginDemo/app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
--- ExpressLoginDemo/routes/index.js
+++ ExpressLoginDemo/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
+});
+*/
+
+router.get('/', function(req, res, next) {
+	res.render('index', {title: 'Express4 Login Demo'});
+});
+
+router.get('/login', function(req, res) {
+	res.render('login', {title: '用户登录'});
+});
+
+router.post('/login', function(req, res) {
+	var user = {
+		username: 'admin',
+		password: 'admin'
+	}
+	if(req.body.username==user.username && req.body.password==user.password) {
+		res.redirect('/home');
+	}
+});
+
+router.get('/logout', function(req, res) {
+	res.redirect('/');
+});
+
+router.get('/home', function(req, res){
+	var user = {
+		username: 'admin',
+		password: 'admin'
+	}
+	res.render('home', {title: 'Home', user: user});
 });
 
 module.exports = router;
