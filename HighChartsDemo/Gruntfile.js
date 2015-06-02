12af08b030bb01242bb9d89bf469aca8b3610a1b
BillyChen <billchen01@163.com>
2015-06-02 22:32:03 +0800

    HighChartsDemo

---
+++ HighChartsDemo/Gruntfile.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(grunt) {
+grunt.initConfig({
+connect: {
+server: {
+options: {
+port: 9001,
+base: './',
+keepalive: true
+}
+}
+}
+});
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.registerTask('server', ['connect']);
+};
